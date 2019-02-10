a74648374ec197b80f39cfda1317e60dbb792543
Georgina Sanchez Buenrostro <46837904+geomsb@users.noreply.github.com>
2019-02-09 22:58:06 -0800

    Take a photo each 30 seconds

--- SnowPhotos.py
+++ SnowPhotos.py
@@ -1,5 +1,6 @@
 from phue import Bridge
 import cv2
+from time import sleep
 
 #Turn on the light
 b = Bridge('192.168.86.21')
@@ -9,6 +10,11 @@ b.set_light(3,'on', True)
 #Take a picture
 
 cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
-cv2.imwrite('C:/geomsb/Test_1.jpg', frame) 
-cap.release()
+
+name = 0
+while True:
+    ret, frame = cap.read()
+    name +=1
+    final_name = 'C:/geomsb/' + str(name) + '.jpg'
+    cv2.imwrite(final_name, frame)
+    sleep(30)  
