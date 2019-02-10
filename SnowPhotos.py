a9f01efda30563a33f827dca30459cf7c5e256c6
Georgina Sanchez Buenrostro <46837904+geomsb@users.noreply.github.com>
2019-02-09 22:26:15 -0800

    Update SnowPhotos.py
    #Comments added

--- SnowPhotos.py
+++ SnowPhotos.py
@@ -1,9 +1,13 @@
 from phue import Bridge
+import cv2
+
+#Turn on the light
 b = Bridge('192.168.86.21')
 b.connect()
 b.set_light(3,'on', True)
 
-import cv2
+#Take a picture
+
 cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 cv2.imwrite('C:/geomsb/Test_1.jpg', frame) 
