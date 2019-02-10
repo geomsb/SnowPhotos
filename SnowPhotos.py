fe9ecbb119839c0efd033ef62ede022065fe4393
Georgina Sanchez Buenrostro <46837904+geomsb@users.noreply.github.com>
2019-02-09 23:09:30 -0800

    Turn off the hue bulb after taking a photo

--- SnowPhotos.py
+++ SnowPhotos.py
@@ -5,16 +5,18 @@ from time import sleep
 #Turn on the light
 b = Bridge('192.168.86.21')
 b.connect()
-b.set_light(3,'on', True)
 
-#Take a picture
-
-cap = cv2.VideoCapture(0)
+#Take a picture each hour
 
 name = 0
 while True:
+    b.set_light(3,'on', True)
+    cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
     name +=1
     final_name = 'C:/geomsb/' + str(name) + '.jpg'
     cv2.imwrite(final_name, frame)
-    sleep(30)  
+    cap.release()
+    b.set_light(3,'on', False)
+    sleep(30)
+      
